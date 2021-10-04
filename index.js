1d9c8422375b211a3abccf4cec2fd12a18e9ee82
Buyiselwa <76174681+Buyiselwa@users.noreply.github.com>
2021-10-04 18:41:02 +0200

    remove folder directories

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var randomNumber1 = Math.floor(Math.random()*6 + 1);
-var randomDice = "images/dice" + randomNumber1 + ".png";
+var randomDice = "dice" + randomNumber1 + ".png";
 var Image1 = document.querySelector(".img1");
 Image1.setAttribute("src", randomDice);
 var randomNumber2 = Math.floor(Math.random()*6 + 1);
-var randomDice2 = "images/dice" + randomNumber2 + ".png";
+var randomDice2 = "dice" + randomNumber2 + ".png";
 document.querySelector(".img2").setAttribute("src", randomDice2);
 
 if (randomNumber1 > randomNumber2) {
